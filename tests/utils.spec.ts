1af66dfe7b18d4c5d056cba842ec4699fb6af50e
Thiago Miranda de Oliveira <thiago.oliveira@bancointer.com.br>
2020-05-21 15:46:45 -0300

    testes funcionais

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -8,16 +8,19 @@ import { convertArrayBufferToString, convertXMLStringToJson } from '../lib/utils
 describe('Sanitize Cep', () => {
   it('Cep com tamanho inválido', () => {
     const invalidCepLength = '123213'
+
     expect(() => sanitizeCep(invalidCepLength)).to.throw()
   })
 
   it('Cep com máscara', () => {
     const maskedCep = '30140-010'
+
     expect(sanitizeCep(maskedCep)).to.be.equals('30140010')
   })
 
   it('Cep sem máscara', () => {
     const notMaskedCep = '30140010'
+
     expect(sanitizeCep(notMaskedCep)).to.be.equals('30140010')
   })
 })
@@ -25,6 +28,7 @@ describe('Sanitize Cep', () => {
 describe('Parsers', () => {
   it('Teste em converter xml para Json', () => {
     const xmlPath = path.join(__dirname, 'static', 'stringpreco.xml')
+
     fs.readFile(xmlPath, 'utf-8', (err, buffer) => {
       const json = convertXMLStringToJson(buffer)
       expect(json).to.be.an('object')
@@ -34,20 +38,22 @@ describe('Parsers', () => {
 
   it('Teste para converter ArrayBuffer para String', () => {
     const htmlPath = path.join(__dirname, 'static', 'rastreio.html')
+
     fs.readFile(htmlPath, 'utf-8', (err, buffer) => {
-      const arrayBuffer = str2ab(buffer)
+      const arrayBuffer = stringToBuffer(buffer)
       const converted = convertArrayBufferToString(arrayBuffer, 'utf-8')
       expect(converted.length).to.be.equals(49934)
       expect(converted).to.be.an('string')
     })
 
-    function str2ab(str) {
-      var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
-      var bufView = new Uint16Array(buf);
-      for (var i=0, strLen=str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
+    function stringToBuffer(str) {
+      const buf = new ArrayBuffer(str.length*2)
+      let bufView = new Uint16Array(buf)
+
+      for (let strIndex=0, strLen=str.length; strIndex < strLen; strIndex++) {
+        bufView[strIndex] = str.charCodeAt(strIndex)
       }
-      return buf;
+      return buf
     }
 
   })
