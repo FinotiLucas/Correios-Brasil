4d01f7f093551f4e5319117b830e413d3159855a
finotilucas <lucas.finoti@protonmail.com>
2021-04-21 12:31:11 -0300

    update

--- lib/utils/URL.ts
+++ lib/utils/URL.ts
@@ -1,6 +1,6 @@
 enum URL {
   BASECEP = 'https://viacep.com.br/ws',
-  BASECORREIOS = 'https://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx',
+  BASECORREIOS = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx',
   BASERASTREIO = 'https://www.linkcorreios.com.br',
   WIDENET = 'https://cep.widenet.host/busca-cep/api/cep',
 }
